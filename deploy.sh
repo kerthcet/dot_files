8a52ab1300c8cc365f5694be899f5684fe84db56
tracy <yn@kid17.com>
2018-10-13 00:35:30 +0800

    💥 增加deploy.sh 文件

---
+++ deploy.sh
@@ -0,0 +1,40 @@
+# vim
+brew install vim
+
+# oh-my-zsh
+git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
+
+# ycm
+brew install yapf
+##############################################
+# cd path/to/ycm
+# ./install.py --go-completer
+##############################################
+
+# tmux
+brew install tmux
+
+# item2
+brew install item2
+
+# python env
+brew install pyenv
+brew install pipenv
+pyenv install -v 3.6.3
+
+# git
+git config --global http.proxy http://127.0.0.1:1087
+git config --global https.proxy https://127.0.0.1:1087
+
+# tools
+brew install autojump
+brew install git-flow
+brew install ansible
+brew install cat
+brew install htop
+brew install tig
+brew install tree
+pip3 install bumpversion
+
+# abandon tools
+# brew install cliclick
