8e3c73b82d96b14d1604c65cfad0962ec5e82c1e
tracy <yn@kid17.com>
2018-11-15 19:25:18 +0800

    ✨ 修改zsh配置

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,10 @@ brew install vim
 # oh-my-zsh
 git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
 
+# wakatime
+pip install wakatime
+git clone https://github.com/wbingli/zsh-wakatime.git ~/.oh-my-zsh/custom/plugins/zsh-wakatime
+
 # ycm
 brew install yapf
 ##############################################
@@ -13,6 +17,13 @@ brew install yapf
 
 # tmux
 brew install tmux
+
+# npm tools
+brew install node
+# vim 格式化json文件等
+npm install -g prettier
+# 基于puppeteer的web截图工具
+npm install site-scan -g
 
 # item2
 brew install item2
