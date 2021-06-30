4ac70c5da9abb577d1da7cb45d0cc1f5f101a6cf
tracy <yn@kid17.com>
2021-06-30 14:08:50 +0800

    add multi configs

--- sync.sh
+++ sync.sh
@@ -1,4 +1,8 @@
 cur=$(pwd)
 echo $cur
 
-cp ~/.bash_aliases $(pwd)/dot_bash_aliases
+cp ~/.bash_aliases $(pwd)/terminal/dot_bash_aliases
+cp ~/.bash_profile $(pwd)/terminal/dot_bash_profile
+cp ~/.tmux.conf $(pwd)/terminal/dot_tmux.conf
+cp ~/.vimrc $(pwd)/terminal/dot_vimrc
+cp ~/.zshrc $(pwd)/terminal/dot_zshrc
