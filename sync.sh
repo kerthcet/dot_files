3e23fb710c50f89cf0e80bcb7ec85330ff41d6ad
kerthcet <kerthcet@gmail.com>
2021-07-08 18:49:11 +0800

    update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,7 +1,8 @@
-cp ~/.bash_aliases $(pwd)/terminal/dot_bash_aliases
-cp ~/.bash_profile $(pwd)/terminal/dot_bash_profile
-cp ~/.tmux.conf $(pwd)/terminal/dot_tmux.conf
-cp ~/.vimrc $(pwd)/terminal/dot_vimrc
-cp ~/.zshrc $(pwd)/terminal/dot_zshrc
+cp ~/.bash_aliases $(pwd)/v2/dot_bash_aliases
+cp ~/.bash_profile $(pwd)/v2/dot_bash_profile
+cp ~/.tmux.conf $(pwd)/v2/dot_tmux.conf
+cp ~/.vimrc $(pwd)/v2/dot_vimrc
+cp ~/.zshrc $(pwd)/v2/dot_zshrc
+cp ~/.ssh/config $(pwd)/v2/dot_ssh_config
 
 cp ~/.vim/UltiSnips/* ./UltiSnips/
