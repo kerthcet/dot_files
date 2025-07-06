6fbfcaacb4e6bf5d560f7bb85a230bc2798289ad
kerthcet <kerthcet@gmail.com>
2025-07-06 16:10:49 +0100

    Remove YCM
    Signed-off-by: kerthcet <kerthcet@gmail.com>

--- sync.sh
+++ sync.sh
@@ -2,5 +2,4 @@ cp -r ~/.bash_aliases $(pwd)/v2/dot_bash_aliases
 cp -r ~/.vimrc $(pwd)/v2/dot_vimrc
 cp -r ~/.zshrc $(pwd)/v2/dot_zshrc
 cp -r ~/.zprofile $(pwd)/v2/dot_zprofile
-cp -r ~/.config $(pwd)/v2/dot_config/
-
+cp -r ~/.config/ghostty $(pwd)/v2/dot_config/ghostty
